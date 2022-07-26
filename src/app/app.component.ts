6b56a2343ef2a5e0e14b108e2bce3c211aede938
Sergio Mena <sergio.mena@tecno.com.mx>
2022-07-26 12:36:18 -0500

    Init commit
    Init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'john doe';
+  todaysDate = new Date();
+  cost = 2000;
+  temperature = 25.123456;
+}
