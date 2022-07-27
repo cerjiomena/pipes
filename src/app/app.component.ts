23cef9d84b20fa0b474c92f862d4aa702f4fa83d
Sergio Mena <sergio.mena@tecno.com.mx>
2022-07-27 14:43:17 -0500

    changes for custom pipe of json.
    changes for custom pipe of json.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ export class AppComponent {
   todaysDate = new Date();
   cost = 2000;
   temperature = 25.123456;
+  pizza = {
+    toppings: ['pepperoni', 'mushroom'],
+    size: 'Large'
+  }
 }
